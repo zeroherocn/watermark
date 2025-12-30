2bcfe749fb86c05707503af99fe319ab4e1296c5
zeroherocn <zero.hero.395@my.csun.edu>
2025-12-30 11:30:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,41 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Unocss from 'unocss/vite'
-import { resolve } from 'path'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import UnoCSS from 'unocss/vite'
+import VueMacros from 'unplugin-vue-macros/vite'
+import path from 'node:path'
 
 export default defineConfig({
-  // 1. 基础路径
-  base: '/watermark/',
-
-  // 2. 插件
-  plugins: [
-    vue(),
-    Unocss()
-  ],
-
-  // 3. 路径别名 (关键修改在这里！)
+  base: "./",
   resolve: {
     alias: {
-      // 告诉 Vite，"@" 和 "~" 都代表 "src" 目录
-      '@': resolve(__dirname, 'src'),
-      '~': resolve(__dirname, 'src'), 
+      '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-
-  // 4. 打包输出
-  build: {
-    outDir: 'dist',
-  }
+  plugins: [
+    VueMacros({
+      plugins: {
+        vue: Vue({
+          reactivityTransform: true,
+        }),
+      },
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        '@vueuse/core',
+        'vue/macros',
+      ],
+      dts: true,
+      dirs: [
+        './src/composables',
+      ],
+      vueTemplate: true,
+    }),
+    Components({
+      dts: true,
+    }),
+    UnoCSS()
+  ]
 })
